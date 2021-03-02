c492a19e17f14ac4fb3d0249c9db049e7799178c
nph <carllegrone@gmail.com>
2021-03-02 18:06:39 -0500

    multi-line default template

--- src/init.rs
+++ src/init.rs
@@ -3,8 +3,21 @@ use std::env;
 use std::fs;
 
 pub fn init() {
-  const DEFAULT_TEMPLATE: &str = "<!DOCTYPE html>\n<html lang=\"en\">\n  <head>\n    <meta charset=\"UTF-8\" />\n    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" />\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />\n    <title>Thingy</title>\n  </head>\n  <body>\n    %body%\n  </body>\n</html>\n";
-  const DEFAULT_INDEX: &str = "Hello World! Edit pages/index.md to edit this page!\n";
+  const DEFAULT_TEMPLATE: &str = "<!DOCTYPE html>
+<html lang=\"en\">
+  <head>
+    <meta charset=\"UTF-8\" />
+    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" />
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />
+    <title>Thingy</title>
+  </head>
+  <body>
+    %body%
+  </body>
+</html>
+";
+  const DEFAULT_INDEX: &str = "Hello World! Edit pages/index.md to edit this page!
+";
 
   let root_dir_pathbuf = env::current_dir().unwrap();
   let root_dir = root_dir_pathbuf.to_str().unwrap();
