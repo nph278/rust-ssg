d0318308958261c1ead71668e79ea8243f593340
nph <carllegrone@gmail.com>
2021-03-03 11:20:44 -0500

    better default title

--- src/init.rs
+++ src/init.rs
@@ -10,7 +10,7 @@ pub fn init() {
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="/main.css" />
-    <title>Thingy</title>
+    <title>Rust-SSG</title>
   </head>
   <body>
     {body}
